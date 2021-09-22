e1c7dd3c26cdc290bb69368aca6efef8d5f61777
fabianTroncoso <troncosoreyes.fabian@gmail.com>
2021-09-21 19:22:49 -0500

    first commit

---
+++ src/main/java/com/tis/mx/ApplicationStarter.java
@@ -0,0 +1,23 @@
+package com.tis.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * The Class ApplicationStarter.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ApplicationStarter {
+
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationStarter.class, args);
+  }
+
+}
